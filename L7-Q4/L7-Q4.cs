6f3a6c43a5ea9f9bd8bceee635f8ca20527739ef
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-02-28 19:28:38 -0300

    att

--- L7-Q4/L7-Q4.cs
+++ L7-Q4/L7-Q4.cs
@@ -4,9 +4,28 @@ namespace L7_Q4
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(MMC(num1, num2));
+        }
+
+        public static int MMC (int x, int y) {
+            int resto = 0;
+            
+            int x1 = x;
+            int y1 = y;
+
+            while (resto != 0) {
+                resto = x1 % y1;
+                x1 = y1;
+                y1 = resto;
+            }
+
+            int mmc = (x1 * y1) / resto;
+            return mmc;
         }
     }
 }
