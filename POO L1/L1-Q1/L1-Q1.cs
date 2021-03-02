0f1c47ba599358f24c5f51644a81e7471f1d64f4
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-03-02 12:11:44 -0300

    questao 2 lista 1

---
+++ POO L1/L1-Q1/L1-Q1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace L1_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo.raio = double.Parse(Console.ReadLine());
+            double areaCirculo = Circulo.calcArea();
+            double circCirculo = Circulo.calcCirc();
+
+            Console.WriteLine($"A area do circulo eh {areaCirculo:.00} e a circunferencia eh {circCirculo:.00}");
+        }
+    }
+
+    class Circulo {
+        public static double raio;
+        public static double calcArea () {
+            double area = Math.PI * Math.Pow(raio, 2);
+            return area;
+        }
+
+        public static double calcCirc () {
+            double circ = Math.PI * raio * 2;
+            return circ;
+        }
+    }
+}
