95e9e20ce257f14c60a34e63004e4d1d288ade2c
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-03-16 20:23:25 -0300

    projeto arael battles

---
+++ LUTA GPLAY/batalha/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace batalha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the battle");
+        }
+    }
+}
