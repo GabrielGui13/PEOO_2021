2ec80fd9b8f6e43e55a7e9f11a850c0cee44eacd
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-03-27 19:44:30 -0300

    q2 finalizada

--- Listas de Exercício/QPOO6_VetoresEMatrizes/ListaPoo6B/Q2/Program.cs
+++ Listas de Exercício/QPOO6_VetoresEMatrizes/ListaPoo6B/Q2/Program.cs
@@ -6,41 +6,48 @@ namespace Q2
     {
         static void Main(string[] args)
         {
-            Bingo x = new Bingo(10);
+            Bingo x = new Bingo(20);
             Console.WriteLine(x.retornarValores());
         }
     }
     class Bingo {
         private int numBolas;
-        private int[] numerosSorteados;
+        private int[] numerosSorteados, arrayAuxiliar;
         private string mostrarSorteados = "[";
         public Bingo (int numBolas) {
             this.numBolas = numBolas;
             numerosSorteados = new int[numBolas];
+            arrayAuxiliar = new int[numBolas];
         }
         public int proximo() {
-            Random x = new Random();
-            int bolaSorteada = x.Next(1, (numBolas + 1));
-            registrarNumeros();
+            Random rand = new Random();
+            int bolaSorteada = rand.Next(1, (numBolas + 1));
 
             return bolaSorteada;
         }
         public void registrarNumeros() {
-            for (int i = 0; i < numerosSorteados.Length; i++) {
-                if (numerosSorteados.Contains(proximo())) continue;
-                else numerosSorteados[i] = proximo();
+            int aux = 0;
+            for (int i = 1; i > 0; i++) {
+                int retornoRand = proximo();
+                if (arrayAuxiliar[retornoRand - 1] == 0) {
+                    arrayAuxiliar[retornoRand - 1] = 1;
+                    numerosSorteados[aux] = retornoRand;
+                    aux++;
+                }
+                if (numerosSorteados[numBolas - 1] != 0) break;
             }
         }
         public int[] sorteados() {
             return this.numerosSorteados;
         } 
         public string retornarValores() {
+            registrarNumeros();
             for (int i = 0; i < numerosSorteados.Length; i++) {
                 if (i != numBolas - 1) mostrarSorteados += numerosSorteados[i] + ", ";
                 else mostrarSorteados += numerosSorteados[i].ToString() + "]";
             }
 
-            return 
+            return mostrarSorteados;
         }
     }
 }
---
+++ Listas de Exercício/QPOO6_VetoresEMatrizes/ListaPoo6B/codigoteste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace codigoteste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numerosSorteados = new int[10];
+
+            Console.WriteLine(numerosSorteados[2]);
+        }
+    }
+}
