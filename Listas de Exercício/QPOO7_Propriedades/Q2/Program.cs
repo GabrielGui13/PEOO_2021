683c9674a7876bff0c8fe690b69557e6cf8532cd
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-03-29 17:09:23 -0300

    q2 lista 7 resolvida

---
+++ Listas de Exercício/QPOO7_Propriedades/Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Frete x = new Frete{Distancia = 585, Peso = 500};
+            Console.WriteLine(x);
+        }
+    }
+    class Frete {
+        private double distancia, peso;
+        public double Distancia {
+            get {return distancia;}
+            set {if (value > 0) distancia = value;}
+        }
+        public double Peso {
+            get {return peso;}
+            set {if (value > 0) peso = value;}
+        }
+        public double valorFrete {
+            get {return peso / 100 * distancia;}
+        }
+        public override string ToString() {
+            return $"Distancia = {distancia} \nPeso = {peso} \nPreço Kg/Km = R$0,01 \nValor = {valorFrete}";
+        }
+    }
+}
