4abd1022946c3dc01bae6eed3e6d66d94723c2a9
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-04-01 21:14:44 -0300

    inicio lista interface I

---
+++ Listas de Exercício/QPOO8_InterfacesI/Q1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno{Nome = "Gabriel", Matricula = "1111", Nascimento = new DateTime(2002, 08, 13)};
+        }
+    }
+    class Aluno : IComparable {
+        public string Nome {
+            get; set;
+        }
+        public string Matricula {
+            get; set;
+        }
+        public DateTime Nascimento {
+            get; set;
+        }
+        public int CompareTo(object obj) {
+            Aluno x = this;
+            Aluno y = (Aluno) obj;
+            return x.Nome.CompareTo(y.Nome);
+        }
+    }
+}
