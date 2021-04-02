d149392d4714765600b0e42b4458351ec90569cc
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-04-02 18:37:32 -0300

    lista de exercicio finalizada

---
+++ Listas de Exercício/QPOO8_InterfacesI/Q2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadroMedalhas quadro = new QuadroMedalhas();
+            Pais brasil = new Pais("Brasil", 3, 0, 0);
+            Pais eua = new Pais("EUA", 3, 0, 2);
+            Pais japao = new Pais("Japão", 3, 0, 1);
+            Pais alemanha = new Pais("Alemanha", 3, 0, 0);
+            Pais coreia = new Pais("Coreia do Sul", 3, 0, 0);
+            quadro.Inserir(brasil);
+            quadro.Inserir(eua);
+            quadro.Inserir(japao);
+            quadro.Inserir(alemanha);
+            quadro.Inserir(coreia);
+            foreach (Pais i in quadro.Listar()) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    class QuadroMedalhas
+    {
+        private Pais[] paises;
+        private int aux;
+        public QuadroMedalhas() {
+            this.paises = new Pais[0];
+        }
+        public void Inserir(Pais p) {
+            if (aux == paises.Length) Array.Resize(ref paises, aux + 1);
+            paises[aux] = p;
+            aux++;
+        }
+        public Pais[] Listar() {
+            Array.Sort(paises);
+            return paises;
+        }
+
+    }
+    class Pais : IComparable {
+        private string nome;
+        private int ouro, prata, bronze;
+        public Pais(string n, int o, int p, int b) {
+            this.nome = n;
+            this.ouro = o;
+            this.prata = p;
+            this.bronze = b;
+        }
+        public int CompareTo(object obj) {
+            Pais pais1 = this;
+            Pais pais2 = (Pais) obj;
+        
+            if (pais1.ouro != pais2.ouro) return -pais1.ouro.CompareTo(pais2.ouro);
+            else if(pais1.ouro == pais2.ouro && pais1.prata != pais2.prata) return -pais1.prata.CompareTo(pais2.prata);
+            else if(pais1.ouro == pais2.ouro && pais1.prata == pais2.prata && pais1.bronze != pais2.bronze) return -pais1.bronze.CompareTo(pais2.bronze);
+            else return pais1.nome.CompareTo(pais2.nome);
+        }
+        public override string ToString() {
+            return $"Pais = {nome}";
+        }
+    }
+}
---
+++ Listas de Exercício/QPOO8_InterfacesI/Q3/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+
+namespace Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aux = 1;
+            Loja gplays = new Loja {Nome = "Gplays"};
+            Aplicativo a1 = new Aplicativo {Nome = "G-Fit", Categoria = "Fitness", Preco = Convert.ToDecimal(1.99), Curtidas = 10};
+            Aplicativo a2 = new Aplicativo {Nome = "Netflix", Categoria = "Multimidia", Preco = Convert.ToDecimal(5.59), Curtidas = 25};
+            Aplicativo a3 = new Aplicativo {Nome = "Facebook", Categoria = "Social", Preco = Convert.ToDecimal(3.99), Curtidas = 5};
+            Aplicativo a4 = new Aplicativo {Nome = "Whatsapp", Categoria = "Social", Preco = Convert.ToDecimal(0.00), Curtidas = 50};
+            gplays.Inserir(a1);
+            gplays.Inserir(a2);
+            gplays.Inserir(a3);
+            gplays.Inserir(a4);
+
+            foreach (Aplicativo i in gplays.Top10MaisCurtidos()) {
+                Console.Write(aux);
+                Console.Write(". ");
+                Console.Write(i);
+                Console.Write("\n");
+                aux++;
+            }
+        }
+    }
+    class Loja {
+        private Aplicativo[] apps;
+        private int k;
+        public string Nome {
+            get; set;
+        }
+        public int Qtd {
+            get {return k;}
+        }
+        public Loja() {
+            this.apps = new Aplicativo[0];
+        }
+        public void Inserir(Aplicativo a) {
+            if (k == apps.Length) Array.Resize(ref apps, k + 1);
+            apps[k] = a;
+            k++;
+        }
+        public void Excluir(Aplicativo a) {
+            int posicao = Array.IndexOf(apps, a);
+            for (int i = posicao; i < apps.Length; i++) {
+                if (i == apps.Length - 1) break;
+                apps[i] = apps[i + 1];
+            }
+            Array.Resize(ref apps, k - 1);
+            k--;
+        }
+        public Aplicativo[] Listar() {
+            Array.Sort(apps);
+            return apps;
+        }
+         public Aplicativo[] Pesquisar(string cat) {
+            int aux = 0;
+
+            for (int i = 0; i < apps.Length; i++) {
+                if (apps[i].Categoria.ToLower() == cat.ToLower()) {
+                    aux++;
+                }
+            }
+
+            Aplicativo[] pesquisa = new Aplicativo[aux];
+            aux = 0;
+
+            for (int i = 0; i < apps.Length; i++) {
+                if(apps[i].Categoria.ToLower() == cat.ToLower()) {
+                    pesquisa[aux] = apps[i];
+                    aux++;
+                }
+            }
+            Array.Sort(pesquisa);
+            return pesquisa;
+        }
+        public Aplicativo[] ListarPreco() {
+            Array.Sort(apps, new PrecoComp());
+            return apps;
+        }
+        public Aplicativo[] Top10MaisCurtidos() {
+            Array.Sort(apps, new CurtidasComp());
+            return apps;
+        }
+    }
+    class Aplicativo : IComparable {
+        private int curtidas;
+        public string Nome {
+            get; set;
+        }
+        public string Categoria {
+            get; set;
+        }
+        public decimal Preco {
+            get; set;
+        }
+        public int Curtidas {
+            set {curtidas = value;}
+            get {return curtidas;}
+        }
+        public void Curtir() {
+            curtidas++;
+        }
+        public int CompareTo(object obj) {
+            Aplicativo x = this;
+            Aplicativo y = (Aplicativo) obj;
+            return x.Nome.CompareTo(y.Nome);
+        }
+        public override string ToString() {
+            return $"{Nome} {Categoria} {Preco} {Curtidas}";
+        }
+    }
+    class PrecoComp : IComparer {
+        public int Compare(object x1, object y1) {
+            Aplicativo x = (Aplicativo) x1;
+            Aplicativo y = (Aplicativo) y1;
+            return x.Preco.CompareTo(y.Preco);
+        }
+    }
+    class CurtidasComp : IComparer {
+        public int Compare(object x1, object y1) {
+            Aplicativo x = (Aplicativo) x1;
+            Aplicativo y = (Aplicativo) y1;
+            return -x.Curtidas.CompareTo(y.Curtidas);
+        }
+    }
+
+}
