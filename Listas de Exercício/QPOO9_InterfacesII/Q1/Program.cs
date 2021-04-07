67e2454d91fa88697b43b64b0aede587550c7883
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-04-07 20:00:12 -0300

    inicio Q1 lista 9

---
+++ Listas de Exercício/QPOO9_InterfacesII/Q1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class DisciplinaAnual : IDisciplina {
+        private string nome;
+        private int nota1, nota2, nota3, nota4, notaFinal;
+        public DisciplinaAnual(string n, int n1, int n2, int n3, int n4) {
+            this.nome = n;
+            this.nota1 = n1;
+            this.nota2 = n2;
+            this.nota3 = n3;
+            this.nota4 = n4;
+        }
+        public string getNome() {
+            return nome;
+        }
+        public int calcMediaParcial() {
+            int media1 = (n1 + n2) * 2 + (n3 + n4) * 3;
+            int media2 = n1 + n2 + n3 + n4;
+            return media1 / media2; 
+        }
+        public int calcMediaFinal() {
+            return;
+        }
+    }
+    class DisciplinaSemestral : IDisciplina {
+        private string nome;
+        private int nota1, nota2, notaFinal;
+        public DisciplinaAnual(string n, int n1, int n2) {
+            this.nome = n;
+            this.nota1 = n1;
+            this.nota2 = n2;
+        }
+        public string getNome() {
+            return nome;
+        }
+        public int calcMediaParcial() {
+            int media1 = n1 * 2 + n2 * 3;
+            int media2 = n1 + n2;
+            return media1 / media2; 
+        }
+        public int calcMediaFinal() {
+            return;
+        }
+    }
+    interface IDisciplina {
+        public string getNome();
+        public int calcMediaParcial();
+        public int calcMediaFinal();
+    }
+
+}
