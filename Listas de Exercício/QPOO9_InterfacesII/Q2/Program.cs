0dfb32af75bda3befa04f6c6becf0c0398a5a805
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-04-08 12:04:38 -0300

    q2 finalizada

--- Listas de Exercício/QPOO9_InterfacesII/Q2/Program.cs
+++ Listas de Exercício/QPOO9_InterfacesII/Q2/Program.cs
@@ -5,9 +5,16 @@ namespace Q2
     class Program
     {
         static void Main(string[] args)
-        {
-            PA x = new PA(2, 5);
-            Console.WriteLine(x.Proximo());
+        {   
+            ISequencia x = new Fibonacci();
+            ISequencia y = new PA(2, 2);
+
+            for (int i = 0; i < 11; i++) {
+                Console.WriteLine(x.Proximo());
+            }
+            for (int i = 0; i < 11; i++) {
+                Console.WriteLine(y.Proximo());
+            }
         }
     }
     class PA : ISequencia {
@@ -25,16 +32,32 @@ namespace Q2
         }
     }
     class Fibonacci : ISequencia {
-        private int primeiroElemento, razao, aux = 1;
+        public int primeiroElemento, razao, e1, e2, e3, aux = 0;
         public Fibonacci () {
             this.primeiroElemento = 0;
-            this.elemento = primeiroElemento;
+            this.e1 = 0;
+            this.e2 = 1;
+            this.razao = e1 + e2;
+            this.e3 = razao;
         }
         public int Iniciar() {
             return primeiroElemento;
         }
         public int Proximo() {
-            
+            if (aux == 0) {
+                aux++;
+                return 0;
+            }
+            else if (aux == 1) {
+                aux++;
+                return 1;
+            }
+            this.e1 = this.e2;
+            this.e2 = this.e3;
+            this.e3 = e1 + e2;
+            this.razao = e3;
+
+            return e3;
         }
     }
     interface ISequencia {
