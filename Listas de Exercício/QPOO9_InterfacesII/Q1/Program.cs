5d27e031d1a511be8d9616bca83f157a2649c11b
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-04-07 21:39:26 -0300

    q2 nao finalizada

--- Listas de Exercício/QPOO9_InterfacesII/Q1/Program.cs
+++ Listas de Exercício/QPOO9_InterfacesII/Q1/Program.cs
@@ -6,7 +6,14 @@ namespace Q1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IDisciplina x = new DisciplinaAnual("Gabriel", 5, 6, 7, 7, 8);
+            IDisciplina y = new DisciplinaSemestral("Gabriel", 7, 2, 9);
+            Console.WriteLine(x.getNome());
+            Console.WriteLine(x.calcMediaParcial());
+            Console.WriteLine(x.calcMediaFinal() + "\n");
+            Console.WriteLine(y.getNome());
+            Console.WriteLine(y.calcMediaParcial());
+            Console.WriteLine(y.calcMediaFinal());
         }
     }
     class DisciplinaAnual : IDisciplina {
@@ -24,18 +31,21 @@ namespace Q1
             return nome;
         }
         public int calcMediaParcial() {
-            int media1 = (n1 + n2) * 2 + (n3 + n4) * 3;
-            int media2 = n1 + n2 + n3 + n4;
-            return media1 / media2; 
+            int media = (nota1 + nota2) * 2 + (nota3 + nota4) * 3;
+            return media / 10; 
         }
         public int calcMediaFinal() {
-            return;
+            return (calcMediaParcial() + notaFinal) / 2;
+        }
+        public override string ToString()
+        {
+            return $" Nota 1 = {nota1}\n Nota 2 = {nota2}\n Nota 3 = {nota3}\n Nota 4 = {nota4}\n Media Parcial = {calcMediaParcial()}\n\n Nota Final = {notaFinal}\n Media Final = {calcMediaFinal()}";
         }
     }
     class DisciplinaSemestral : IDisciplina {
         private string nome;
         private int nota1, nota2, notaFinal;
-        public DisciplinaAnual(string n, int n1, int n2, int nf) {
+        public DisciplinaSemestral(string n, int n1, int n2, int nf) {
             this.nome = n;
             this.nota1 = n1;
             this.nota2 = n2;
@@ -45,18 +55,20 @@ namespace Q1
             return nome;
         }
         public int calcMediaParcial() {
-            int media1 = n1 * 2 + n2 * 3;
-            int media2 = n1 + n2;
-            return media1 / media2; 
+            int media = nota1 * 2 + nota2 * 3;
+            return media / 5;
         }
         public int calcMediaFinal() {
-            return;
+            return (calcMediaParcial() + notaFinal) / 2;
+        }
+        public override string ToString() {
+            return $" Nota 1 = {nota1}\n Nota 2 = {nota2}\n Media Parcial = {calcMediaParcial()}\n\n Nota Final = {notaFinal}\n Media Final = {calcMediaFinal()}";
         }
     }
     interface IDisciplina {
-        public string getNome();
-        public int calcMediaParcial();
-        public int calcMediaFinal();
+        string getNome();
+        int calcMediaParcial();
+        int calcMediaFinal();
     }
 
 }
---
+++ Listas de Exercício/QPOO9_InterfacesII/Q2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci x = new Fibonacci();
+            
+        }
+    }
+    class PA : ISequencia {
+        private int primeiroElemento, razao, aux = 1;
+        public PA (int p, int r) {
+            this.primeiroElemento = p;
+            this.razao = r;
+        }
+        public int Iniciar() {
+            return primeiroElemento;
+        }
+        public int Proximo() {
+            return primeiroElemento + razao * aux;
+            aux++;
+        }
+    }
+    class Fibonacci : ISequencia {
+        private int primeiroElemento, razao, aux = 1;
+        public Fibonacci () {
+            this.primeiroElemento = 0;
+            this.elemento = primeiroElemento;
+            this.razao = (elemento - 1) + (elemento - 2);
+        }
+        public int Iniciar() {
+            return primeiroElemento;
+        }
+        public int Proximo() {
+            if (elemento == 0) {
+                return 0;
+                this.elemento = 1;
+            }
+            else if (elemento == 1) {
+                return 1;
+                this.elemento = 2;
+            }
+            else {
+                return novoElemento;
+                elemento = novoElemento;
+            }
+        }
+    }
+    interface ISequencia {
+        int Iniciar();
+        int Proximo();
+    }
+}
