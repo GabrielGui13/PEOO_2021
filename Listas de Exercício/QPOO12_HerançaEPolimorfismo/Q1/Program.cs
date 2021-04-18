a585607acbe82683ac9e7c212e47bbe531fb809e
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-04-18 00:20:02 -0300

    lista 12 finalizada codigo da q2 mais ou menos

---
+++ Listas de Exercício/QPOO12_HerançaEPolimorfismo/Q1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo(3, 4);
+            Console.WriteLine(x);
+            Quadrado y = new Quadrado(10);
+            Console.WriteLine(y);
+            Retangulo z = new Quadrado(5);
+            Console.WriteLine(z);
+        }
+    }
+    class Retangulo {
+        private double b, h;
+        public Retangulo(double b, double h) {
+            this.b = b;
+            this.h = h;
+        }
+        public double Base {
+            get => b;
+        }
+        public double Altura {
+            get => h;
+        }
+        public double Area {
+            get => b * h;
+        }
+        public double Diagonal {
+            get => Math.Sqrt(Math.Pow(b, 2) + Math.Pow(h, 2));
+        }
+        public override string ToString()
+        {
+            return $"Base = {Base} \nAltura = {Altura} \nArea = {Area} \nDiagonal = {Diagonal}\n";
+        }
+    }
+    class Quadrado : Retangulo {
+        public Quadrado(double l) : base(l, l) {}
+        public override string ToString()
+        {
+            return $"Base = {Base} \nAltura = {Altura} \nArea = {Area} \nDiagonal = {Diagonal:.00}\n";
+        }
+    }
+}
---
+++ Listas de Exercício/QPOO12_HerançaEPolimorfismo/Q2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Frete x = new Frete(600, 5000);
+            Console.WriteLine(x);
+            FreteExpresso y = new FreteExpresso(300, 7000, 1000);
+            Console.WriteLine(y);
+        }
+    }
+    class Frete {
+        protected double distancia, peso;
+        public virtual decimal ValorFrete {
+            get => Convert.ToDecimal(distancia * peso * 0.01);
+        }
+        public Frete (double d, double p) {
+            this.distancia = d;
+            this.peso = p;
+        }
+        public override string ToString(){
+            return $"Distancia = {distancia} \nPeso = {peso} \nValor = {ValorFrete}\n";
+        }
+    }
+    class FreteExpresso : Frete {
+        private decimal seguro;
+        public override decimal ValorFrete {
+            get => base.ValorFrete * Convert.ToDecimal(2) + (seguro * Convert.ToDecimal(0.1));
+        } 
+        public FreteExpresso(double d, double p, decimal s) : base (d, p) {
+            this.seguro = s;
+        }
+        public override string ToString(){
+            return $"Distancia = {distancia} \nPeso = {peso} \nSeguro = {seguro} \nValor = {ValorFrete}\n";
+        }
+    }
+}
