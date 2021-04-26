210ec5d67ede26644264a4694195002a573d8c5d
GabrielGui13 <gfabrielguilherme13@gmail.com>
2021-04-26 19:53:39 -0300

    pasta qpoo13

---
+++ potibosta/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace potibosta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = {30, 17, 16, 32, 20, 28, -8, 33, 1, -1};
+            int soma = 0;
+
+            for (int i = 0; i < num.Length; i++) {
+                if (i % 2 == 0) soma += num[i];
+                else soma -= num[i];
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
