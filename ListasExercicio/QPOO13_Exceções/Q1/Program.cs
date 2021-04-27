b0d9bdc45c7c85d8db2731a08123c1128742b1e7
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-04-26 23:48:22 -0300

    Q1

--- ListasExercicio/QPOO13_Exceções/Q1/Program.cs
+++ ListasExercicio/QPOO13_Exceções/Q1/Program.cs
@@ -6,7 +6,29 @@ namespace Q1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Retangulo x = new Retangulo();
+            try {
+                Console.WriteLine("Base");
+                x.setBase(double.Parse(Console.ReadLine()));
+                Console.WriteLine("Altura");
+                x.setAltura(double.Parse(Console.ReadLine()));
+
+                Console.WriteLine(x);
+            }
+            catch (ArgumentOutOfRangeException e) {
+                Console.WriteLine("Digite um numero acima de 0");
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e) {
+                Console.WriteLine("Numero invalido");
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception) {
+                Console.WriteLine("Ocorreu algum erro tente novamente");
+            }
+            finally {
+                Console.WriteLine("Fim");
+            }
         }
     }
     class Retangulo {
@@ -19,7 +41,21 @@ namespace Q1
         }
         public void setBase(double b) {
             if (b > 0) this.b = b;
-            else throw new ArgumentOutOfRangeException();
+            else throw new ArgumentOutOfRangeException("Numero negativo");
+        }
+        public double getBase() {
+            return b;
+        }
+        public void setAltura(double h) {
+            if (h > 0) this.h = h;
+            else throw new ArgumentOutOfRangeException("Numero negativo");
+        }
+        public double getAltura() {
+            return h;
+        }
+        public override string ToString()
+        {
+            return $"Base = {getBase()} \nAltura = {getAltura()} \nArea = {Area} \nDiagonal = {Diagonal:.00}";
         }
     }
 }
