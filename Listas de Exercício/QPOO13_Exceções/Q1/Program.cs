78933ee7c5b75a21b3b434c61e7421030d371933
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-04-26 23:28:05 -0300

    correcoes

--- Listas de Exercício/QPOO13_Exceções/Q1/Program.cs
+++ Listas de Exercício/QPOO13_Exceções/Q1/Program.cs
@@ -9,4 +9,18 @@ namespace Q1
             Console.WriteLine("Hello World!");
         }
     }
+    class Retangulo {
+        private double b, h;
+        public double Area {
+            get {return b * h;}
+        }
+        public double Diagonal {
+            get {return Math.Sqrt(Math.Pow(b, 2) + Math.Pow(h, 2)); }
+        }
+        public void setBase(double b) {
+            if (b > 0) this.b = b;
+            else throw new ArgumentOutOfRangeException();
+        }
+        public double 
+    }
 }
